5db8820ea858130752d5e2ab5542788a1672a482
Torin Kogut <torin.kogut@yahoo.com>
2013-09-10 18:44:11 -0600

    Done a bunch of random stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
